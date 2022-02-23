e1c2640085003fbb7c0e7932df98f75772c9773a
meenpimchaya <94286593+meenpimchaya@users.noreply.github.com>
2022-02-24 01:51:59 +0700

    phpbasic

---
+++ hw1.php
@@ -0,0 +1,25 @@
+<?php
+
+    $a=1;
+    $b=1;
+    if ($a = $b){
+        echo "Hello";
+    }
+
+    echo("<br>");
+    if($a>=$b){
+        echo "Happy";
+    }else {
+        echo "Sad";
+    }
+
+    echo("<br>");
+    if($a>$b){
+        echo "Wow!";
+    }elseif($a<$b) {
+        echo "Yahoo!";
+    }else{
+        echo "hahaha";
+    }
+
+?>
